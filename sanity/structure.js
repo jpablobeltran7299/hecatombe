980e4c4d313a09cc2b171f1579ea76abc00a207e
pablogonzalezneruc <pablo.gonzalez@gruponeruc.com>
2026-05-20 00:11:23 -0600

    fix: estructura Studio sin marca ni categoria

--- sanity/structure.js
+++ sanity/structure.js
@@ -1,5 +1,11 @@
-// https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure = (S) =>
   S.list()
     .title('Content')
-    .items(S.documentTypeListItems())
+    .items([
+      S.documentTypeListItem('producto').title('Producto'),
+      S.documentTypeListItem('banner').title('Banners'),
+      S.documentTypeListItem('dinamica').title('Dinámica'),
+      S.documentTypeListItem('tematica').title('Temática'),
+      S.documentTypeListItem('linea').title('Línea de producto'),
+      S.documentTypeListItem('universo').title('Universo'),
+    ])
